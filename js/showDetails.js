74f2f1314246564e925db86858bc1361f46c1409
shamratPG <shamratpg@gmail.com>
2022-09-03 23:59:19 +0600

    Final commit

--- js/showDetails.js
+++ js/showDetails.js
@@ -14,14 +14,15 @@ function showDetails(data) {
     modalContent.innerHTML = `
         <div class="modal-header">
             <h5 class="modal-title" id="show-detailsLabel">${newsTitle}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button type="button" class="btn-close p-3" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="">
-            <img class="img-fluid" src = "${data.image_url}">
+            <img class="w-100" src = "${data.image_url}">
         </div>
-        <div class="ps-3 pt-2 d-flex align-items-center">
+        <div class="mt-3 ps-3 pt-2 d-flex align-items-center">
             <img class="img-fluid rounded-circle" src="${data.author.img}" alt="Author Image" style="height: 40px; width: auto;">
             <span class="ps-3 text-secondary">${authorName}</span>
+            
             <div class="text-primary ps-4">
                 <i class="fa-solid fa-eye pe-1"></i>
                 <span>${viewCount}</span>
